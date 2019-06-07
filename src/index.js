37056808111f2ea180ef516b2eb7aaf13bb1e95e
Daniel Hernandez <danielandres65@gmail.com>
2019-06-07 11:54:01 -0500

    Packages installed and removed

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import 'jquery-multifile';
+import 'icheck/icheck';
+
 
 
 ReactDOM.render(<App />, document.getElementById('root'));
