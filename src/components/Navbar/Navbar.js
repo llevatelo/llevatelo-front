3865deb5f19d4709181230fc1170e52192da837c
Nicolas Mena <nmena.garzon@gmail.com>
2019-07-20 19:57:13 -0500

    Change item detail component (#7)
    * Remove components in deprecated
    
    * change copies

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -12,63 +12,59 @@ class Navbar extends Component {
   render() {
     return (
       <div className="page home-page">
-        <div className="hero-wrapper">
-          <SecondaryNavigation />
-          {/*============ Main Navigation ====================================================================*/}
-          <div className="main-navigation">
-            <div className="container">
-              <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
-                <a className="navbar-brand" href="/home">
-                  <img src={Logo} alt="Logo" />
-                </a>
-                <button
-                  className="navbar-toggler"
-                  type="button"
-                  data-toggle="collapse"
-                  data-target="/navbar"
-                  aria-controls="navbar"
-                  aria-expanded="false"
-                  aria-label="Toggle navigation"
-                >
-                  <span className="navbar-toggler-icon" />
-                </button>
-                <div className="collapse navbar-collapse" id="navbar">
-                  {/*Main navigation list*/}
-                  <ul className="navbar-nav">
-                    <li className="nav-item active">
-                      <Link to="/login" className='nav-link'>Ingresar</Link>
-                    </li>
+        <SecondaryNavigation />
+        <div className="main-navigation">
+          <div className="container">
+            <nav className="navbar navbar-expand-lg navbar-light justify-content-between">
+              <a className="navbar-brand" href="/home">
+              <img src={Logo} alt="Logo" />
+              </a>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-toggle="collapse"
+                data-target="/navbar"
+                aria-controls="navbar"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon" />
+              </button>
+              <div className="collapse navbar-collapse" id="navbar">
+                {/*Main navigation list*/}
+                <ul className="navbar-nav">
+                  <li className="nav-item active">
+                    <Link to="/login" className='nav-link'>Ingresar</Link>
+                  </li>
 
-                    <li className="nav-item">
-                      <Link to="/register" className='nav-link'>Registrarse</Link>
-                    </li>
+                  <li className="nav-item">
+                    <Link to="/register" className='nav-link'>Registrarse</Link>
+                  </li>
 
-                    <li className="nav-item">
-                      <Link to="/faq" className='nav-link'>Ayuda</Link>
-                    </li>
-                    <li className="nav-item">
-                      <a
-                        href="/create-article"
-                        className="btn btn-primary text-caps btn-rounded btn-framed"
-                      >
-                        Publicar Artículo
-                      </a>
-                    </li>
-                  </ul>
-                  {/*Main navigation list*/}
-                </div>
-                {/*end navbar-collapse*/}
-              </nav>
-              {/*end navbar*/}
-            </div>
-            {/*end container*/}
-          </div>
-          <div className="background" />
+                  <li className="nav-item">
+                    <Link to="/faq" className='nav-link'>Ayuda</Link>
+                  </li>
+                  <li className="nav-item">
+                    <a
+                      href="/create-article"
+                      className="btn btn-primary text-caps btn-rounded btn-framed"
+                    >
+                      Publicar Artículo
+                    </a>
+                  </li>
+                </ul>
+                {/*Main navigation list*/}
+              </div>
+              {/*end navbar-collapse*/}
+            </nav>{/*end navbar*/}
+          </div> {/*end container*/}
+        </div> {/*end main navigation*/}
+
+        <div className="background" />
           <div className="background-image original-size">
             <img src={BackgroundIcons} alt="" />
           </div>
         </div>
-      </div>
     );
   }
 }
